2099dba4d51f3a01e7a3f2b14ba18885dff7eafd
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-05-28 09:02:36 +0200

    feat: add db index on nft.ownerId

--- packages/core-database-postgres/src/migrations/20190516100000-create-nfts-table.sql
+++ packages/core-database-postgres/src/migrations/20190516100000-create-nfts-table.sql
@@ -3,3 +3,6 @@ CREATE TABLE IF NOT EXISTS ${schema~}.nfts (
     "id" VARCHAR(64) PRIMARY KEY,
     "owner_id" VARCHAR(36)
 );
+
+-- Constraints
+CREATE INDEX IF NOT EXISTS "nfts_owner_id" ON nfts ("owner_id");

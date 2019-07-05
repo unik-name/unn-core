273ce823e2af037b476ef98db1390d453bc98d54
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-07-05 16:17:29 +0200

    fix: update cron script to fit new installation process (CLI)

--- scripts/upstream/update-upstream.sh
+++ scripts/upstream/update-upstream.sh
@@ -2,13 +2,11 @@
 
 set -e
 
-CORE_PATH=~/unikname-core
-CORE_DEPLOYER_PATH=~/unikname-deployer
+cd /home/unikname/core
 
 echo -e "\n["$(date +%F_%H-%M-%S)"]"
 
 echo -e "\nFetching sources"
-cd $CORE_PATH
 git fetch
 DIFF=$(git diff origin/private/develop --summary)
 
@@ -17,25 +15,24 @@ then
     echo "--> no updates."
     exit 0
 else 
-    git pull origin private/develop
+    git pull -s recursive -X theirs origin private/develop
     echo "--> source updated"
 fi
 
 cp ./scripts/upstream/check-uns-update /etc/cron.d/
 
 echo -e "\nStopping node"
-cd $CORE_DEPLOYER_PATH
-./bridgechain.sh stop-core --config unikname.config.json --network devnet
+uns relay:stop 
+uns forger:stop
 echo "--> Node stopped"
 
 echo -e "\nBuilding sources"
-cd $CORE_PATH
 rm -rf ./node_modules
 yarn && yarn setup:clean
 echo "--> sources built"
 
 echo -e "\nStarting node"
-cd $CORE_DEPLOYER_PATH
-./bridgechain.sh start-core --config unikname.config.json --network devnet
+uns relay:start --network=devnet --ignoreMinimumNetworkReach
+uns forger:start --network=devnet
 
 exit 0

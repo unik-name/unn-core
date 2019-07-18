88b14757cdd04d4a76b5cc6d43d587c60efa54cc
Fabien TREGUER <fabien.treguer@spacelephant.org>
2019-07-18 11:48:42 +0200

    test: Add unit and integration tests

---
+++ __tests__/integration/core-tester-cli/commands/send/nftupdate.test.ts
@@ -0,0 +1,66 @@
+import { httpie } from "@arkecosystem/core-utils";
+import { TransactionTypes } from "@arkecosystem/crypto/dist/constants";
+import "jest-extended";
+import nock from "nock";
+import { NFTUpdateCommand } from "../../../../../packages/core-tester-cli/src/commands/send/nftupdate";
+import { feeManager } from "../../../../../packages/crypto/src/managers/fee";
+import { arkToSatoshi, captureTransactions, expectTransactions, toFlags } from "../../shared";
+
+const TOKEN_ID = "ee16f4b75f38f6e3d16635f72a8445e0ff8fbacfdfa8f05df077e73de79d6e4f";
+const OWNER_PASSPHRASE = "clay harbor enemy utility margin pretty hub comic piece aerobic umbrella acquire";
+let opts: any;
+let expectedFee: number;
+
+beforeEach(async () => {
+    // Just passthru. We'll test the Command class logic in its own test file more thoroughly
+    nock("http://localhost:4003")
+        .get("/api/v2/node/configuration")
+        .twice()
+        .reply(200, { data: { constants: {} } });
+
+    nock("http://localhost:4000")
+        .get("/config")
+        .twice()
+        .reply(200, { data: { network: { name: "unitnet" } } });
+
+    // Mock nft get
+    nock("http://localhost:4003")
+        .get(`/api/v2/nfts/${TOKEN_ID}`)
+        .reply(200, { data: { constants: {} } });
+
+    jest.spyOn(httpie, "post");
+
+    expectedFee = feeManager.get(TransactionTypes.NftUpdate);
+    opts = {
+        id: TOKEN_ID,
+        nftFee: expectedFee,
+        props: '{"myProp": "myValue", "test": null}',
+        owner: OWNER_PASSPHRASE,
+    };
+});
+
+afterEach(() => {
+    nock.cleanAll();
+});
+
+describe("Commands - NftUpdate", () => {
+    it("should post NftUpdate Transaction", async () => {
+        const expectedTransactions = [];
+        captureTransactions(nock, expectedTransactions);
+        await NFTUpdateCommand.run(toFlags(opts));
+
+        expectTransactions(expectedTransactions, {
+            amount: 0,
+            fee: arkToSatoshi(expectedFee),
+            asset: {
+                nft: {
+                    tokenId: TOKEN_ID,
+                    properties: {
+                        myProp: "myValue",
+                        test: null,
+                    },
+                },
+            },
+        });
+    });
+});
---
+++ __tests__/unit/crypto/builder/transactions/nft/nft-update.test.ts
@@ -0,0 +1,56 @@
+import "jest-extended";
+import { NFTUpdateBuilder } from "../../../../../../packages/crypto/src/builder/transactions/nft/nft-update";
+import { client } from "../../../../../../packages/crypto/src/client";
+import { TransactionTypes } from "../../../../../../packages/crypto/src/constants";
+import { feeManager } from "../../../../../../packages/crypto/src/managers/fee";
+import { Transaction } from "../../../../../../packages/crypto/src/transactions";
+
+let builder: NFTUpdateBuilder;
+let properties: { [_: string]: string };
+
+const TOKEN_ID = "6f35a40470021425558f5cbb7b5f056e51b694db5cc6c336abdc6b777fc9d051";
+const SENDER_PK = "03287bfebba4c7881a0509717e71b34b63f31e40021c321f89ae04f84be6d6ac37";
+const OWNER_PASSPHRASE = "clay harbor enemy utility margin pretty hub comic piece aerobic umbrella acquire";
+
+beforeEach(() => {
+    builder = client.getBuilder().nftUpdate(TOKEN_ID);
+    properties = {
+        myPropKey: "propValue",
+        myPropKey2: "propValue2",
+        myProp2Delete: null,
+    };
+    builder
+        .properties(properties)
+        .senderPublicKey(SENDER_PK)
+        .sign(OWNER_PASSPHRASE);
+});
+
+describe("NFTUpdate Transaction", () => {
+    describe("verify", () => {
+        it("should be valid with a signature", () => {
+            expect(builder.build().verified).toBeTrue();
+            expect(builder.verify()).toBeTrue();
+        });
+    });
+
+    describe("getStruct", () => {
+        it("should have its specific properties", () => {
+            expect(builder).toHaveProperty("data.type", TransactionTypes.NftUpdate);
+            expect(builder).toHaveProperty("data.fee", feeManager.get(TransactionTypes.NftUpdate));
+            expect(builder).toHaveProperty("data.senderPublicKey", SENDER_PK);
+            expect(builder).toHaveProperty("data.asset.nft.properties", properties);
+        });
+
+        it("should return all properties", () => {
+            const transactionStruct = builder.getStruct();
+            expect(transactionStruct).toHaveProperty("asset.nft.properties", properties);
+        });
+
+        it("should return all properties after build", () => {
+            const transaction: Transaction = builder.build();
+            // Has to not be replaced by undefined, stay null (property is removed from structure when value is undefined)
+            expect(transaction.data.asset.nft.properties.myProp2Delete).toBeDefined();
+            expect(transaction.data.asset.nft.properties.myProp2Delete).toBeNull();
+        });
+    });
+});

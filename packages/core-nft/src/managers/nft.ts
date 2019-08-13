b3a72e9945e2679c108fedf9d96ed5a4673bb60a
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-08-13 14:15:46 +0200

    feat: nft-mint tx properties is optional

--- packages/core-nft/src/managers/nft.ts
+++ packages/core-nft/src/managers/nft.ts
@@ -28,12 +28,14 @@ export class NFTManager implements NFT.INFTManager {
 
     public async applyConstraints(transaction: ITransactionData) {
         const { properties } = transaction.asset.nft;
-        for (const [key, value] of Object.entries(properties)) {
-            await this.constraintsManager.getAndApplyConstraints({
-                propertyKey: key,
-                propertyNewValue: value,
-                transaction,
-            });
+        if (properties) {
+            for (const [key, value] of Object.entries(properties)) {
+                await this.constraintsManager.getAndApplyConstraints({
+                    propertyKey: key,
+                    propertyNewValue: value,
+                    transaction,
+                });
+            }
         }
     }
 }
--- packages/core-tester-cli/src/commands/send/nftmint.ts
+++ packages/core-tester-cli/src/commands/send/nftmint.ts
@@ -1,11 +1,11 @@
 import { Address } from "@arkecosystem/crypto";
-import { NFTUPDATE_FLAGS, NFTUpdateCommand } from "./nftupdate";
+import { getNftUpdateFlags, NFTUpdateCommand } from "./nftupdate";
 
 export class NFTMintCommand extends NFTUpdateCommand {
     public static description: string = "mint a non-fungible token and set properties";
 
     // TODO why is it required ? Crashes at runtime: unexpected arguments
-    public static flags = NFTUPDATE_FLAGS;
+    public static flags = getNftUpdateFlags(false);
 
     protected async createWalletsWithBalance(flags: Record<string, any>): Promise<any[]> {
         const ownerAddress = Address.fromPassphrase(flags.owner);

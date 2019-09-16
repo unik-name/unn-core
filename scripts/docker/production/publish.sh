521522fdf51878b7c35490385a47e56800a1c11a
Damien Lecan <dev@dlecan.com>
2019-09-16 15:16:53 +0200

    build(docker): improve publish task not to tag 'latest' as they aren't required anymore

--- scripts/docker/production/publish.sh
+++ scripts/docker/production/publish.sh
@@ -7,9 +7,7 @@ RELEASE="${1}"
 COMMIT=$(git rev-parse --short HEAD)
 
 docker tag "$IMAGE":"$COMMIT" "$IMAGE":"$RELEASE"
-docker tag "$IMAGE":"$COMMIT" "$IMAGE":latest
 
 docker push "$IMAGE":"$RELEASE"
-docker push "$IMAGE":latest
 
 echo "🎉 Successfully published UNS image : $IMAGE:$RELEASE"

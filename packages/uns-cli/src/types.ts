6d3449856aee216ef78a552b0e95db753b43bb83
Fabien TREGUER <fabien.treguer@spacelephant.org>
2019-10-02 16:35:57 +0200

    feat: remove cli from uns-core repository transfered to https://github.com/unik-name/uns-cli

--- packages/uns-cli/src/types.ts
+++
@@ -1,9 +0,0 @@
-import { DIDHelpers, DIDTypes } from "@uns/ts-sdk";
-
-export const getUnikTypesList = () => {
-    return DIDHelpers.labels().map(type => type.toLowerCase());
-};
-
-export const getTypeValue = (tokenType: string): string => {
-    return `${DIDTypes[tokenType.toUpperCase()]}`;
-};
--- packages/uns-cli/src/writeCommand.ts
+++
@@ -1,9 +0,0 @@
-import { BaseCommand } from "./baseCommand";
-import { feeFlag } from "./utils";
-
-export abstract class WriteCommand extends BaseCommand {
-    public static flags = {
-        ...BaseCommand.baseFlags,
-        ...feeFlag(),
-    };
-}

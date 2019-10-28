1487ccd33fd716d8f30a2c6358fc5c6a36218744
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-10-28 16:31:27 +0100

    chores: remove ci config generation on pre-commit hook

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,7 +2,7 @@
 
 echo "Running pre-commit script..."
 
-node .circleci/generateConfig.js
-git add .circleci/config.yml
+# node .circleci/generateConfig.js
+# git add .circleci/config.yml
 
 echo "pre-commit script was run succesfully"

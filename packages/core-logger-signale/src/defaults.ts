3d157173d4af3f955b3dad96894f6eb6b54bb576
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-11-08 16:03:54 +0100

    feat: improve signale logger

--- packages/core-logger-signale/src/defaults.ts
+++ packages/core-logger-signale/src/defaults.ts
@@ -1,3 +1,16 @@
 export const defaults = {
     logLevel: process.env.CORE_LOG_LEVEL || "info",
+    config: {
+        displayDate: true,
+        displayTimestamp: true,
+        displayBadge: false,
+    },
+    types: {
+        info: {
+            color: "green",
+        },
+        debug: {
+            color: "cyan",
+        },
+    },
 };

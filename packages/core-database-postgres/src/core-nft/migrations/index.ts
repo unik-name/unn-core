f130ed9129bf759a99de9b5f5a5d839a86f88cd0
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-11-19 14:29:12 +0100

    fix(core-nft): import nft migration queries instead of file paths

--- packages/core-database-postgres/src/core-nft/migrations/index.ts
+++ packages/core-database-postgres/src/core-nft/migrations/index.ts
@@ -1,3 +1,7 @@
 import { readdirSync } from "fs";
+import { loadQueryFile } from "../../utils";
 
-export const migrationPaths = readdirSync(__dirname).map(path => "../core-nft/migrations/".concat(path));
+export const migrations = readdirSync(__dirname)
+    .filter(name => name.substr(-4).toLowerCase() === ".sql")
+    .sort()
+    .map(name => loadQueryFile(__dirname, name));
--- packages/core-database-postgres/src/migrations/index.ts
+++ packages/core-database-postgres/src/migrations/index.ts
@@ -1,11 +1,12 @@
 import { readdirSync } from "fs";
 import { loadQueryFile } from "../utils";
 
-import { migrationPaths } from "../core-nft";
+import { migrations as nftMigrations } from "../core-nft";
 
 export const migrations = readdirSync(__dirname)
-    // TODO: uns : find better way to load migrations
-    .concat(migrationPaths)
     .filter(name => name.substr(-4).toLowerCase() === ".sql")
     .sort()
-    .map(name => loadQueryFile(__dirname, name));
+    .map(name => loadQueryFile(__dirname, name))
+
+    // TODO: uns : find better way to load migrations
+    .concat(nftMigrations);

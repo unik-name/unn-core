b63c25e28ef0b75c51eabc000e2b4301282453d4
Fabien TREGUER <fabien.treguer@spacelephant.org>
2019-11-26 11:03:18 +0100

    test imports

--- __tests__/unit/uns-crypto/builder/nft-transfer.test.ts
+++ __tests__/unit/uns-crypto/builder/nft-transfer.test.ts
@@ -2,18 +2,18 @@ import "jest-extended";
 const TOKEN_ID = "6f35a40470021425558f5cbb7b5f056e51b694db5cc6c336abdc6b777fc9d051";
 const RECEIVER = "APyFYXxXtUrvZFnEuwLopfst94GMY5Zkeq";
 
-import { NftBuilderFactory, NftTransactions, NFTTransferBuilder } from "@uns/core-nft-crypto";
+import { NftBuilders, NftTransactions } from "@uns/core-nft-crypto";
 
 import { Managers, Transactions } from "@arkecosystem/crypto";
 
-let builder: NFTTransferBuilder;
+let builder: NftBuilders.NftTransferBuilder;
 
 describe("Business update builder", () => {
     Managers.configManager.setFromPreset("testnet");
-    Transactions.TransactionRegistry.registerTransactionType(NftTransactions.NFTTransferTransaction);
+    Transactions.TransactionRegistry.registerTransactionType(NftTransactions.NftTransferTransaction);
 
     beforeEach(() => {
-        builder = NftBuilderFactory.nftTransfer(TOKEN_ID);
+        builder = new NftBuilders.NftTransferBuilder("unik", TOKEN_ID);
     });
 
     describe("should test verification", () => {
--- packages/uns-crypto/src/index.ts
+++ packages/uns-crypto/src/index.ts
@@ -3,3 +3,4 @@ export * from "./transactions";
 export * from "./interfaces";
 export * from "./enums";
 export * from "./crypto";
+export * from "./models";

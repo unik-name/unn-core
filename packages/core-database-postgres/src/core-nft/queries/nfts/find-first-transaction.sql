eb4c909100b34b181bbf01bcf863f7816b7ee7c3
Fabien TREGUER <fabien.treguer@spacelephant.org>
2019-11-26 11:00:04 +0100

    upgrade groupe number in sql files

--- packages/core-database-postgres/src/core-nft/queries/nfts/find-first-transaction.sql
+++ packages/core-database-postgres/src/core-nft/queries/nfts/find-first-transaction.sql
@@ -1,7 +1,7 @@
 SELECT id
 FROM transactions
 WHERE type IN (0,1,2)
-AND transactions.type_group = 1337
+AND transactions.type_group = 2000
 AND asset -> 'nft' -> 'unik' ->> 'tokenId' = ${id}
 ORDER BY transactions.timestamp ASC, transactions.sequence ASC
 LIMIT 1

e64b9ecf84f8a6b555b840b9b3466494565c9d72
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-11-28 10:02:02 +0100

    test: fix imports

--- __tests__/unit/uns-crypto/builder/nft-transfer.test.ts
+++ __tests__/unit/uns-crypto/builder/nft-transfer.test.ts
@@ -2,18 +2,18 @@ import "jest-extended";
 const TOKEN_ID = "6f35a40470021425558f5cbb7b5f056e51b694db5cc6c336abdc6b777fc9d051";
 const RECEIVER = "APyFYXxXtUrvZFnEuwLopfst94GMY5Zkeq";
 
-import { NftBuilders, NftTransactions } from "@uns/core-nft-crypto";
+import { Builders, Transactions as NftTransactions } from "@uns/core-nft-crypto";
 
 import { Managers, Transactions } from "@arkecosystem/crypto";
 
-let builder: NftBuilders.NftTransferBuilder;
+let builder: Builders.NftTransferBuilder;
 
 describe("NftTransfer builder", () => {
     Managers.configManager.setFromPreset("testnet");
     Transactions.TransactionRegistry.registerTransactionType(NftTransactions.NftTransferTransaction);
 
     beforeEach(() => {
-        builder = new NftBuilders.NftTransferBuilder("unik", TOKEN_ID);
+        builder = new Builders.NftTransferBuilder("unik", TOKEN_ID);
     });
 
     describe("should test verification", () => {

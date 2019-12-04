4d9fb2ab5c7dd8d6d4af606c6d9de215e97be502
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-12-04 17:36:46 +0100

    test(core-nft): fix imports

--- __tests__/unit/core-nft/constraints/manager.test.ts
+++ __tests__/unit/core-nft/constraints/manager.test.ts
@@ -6,50 +6,50 @@ import { Transactions as NFTTransactions } from "../../../../packages/core-nft-c
 import { ConstraintsManager } from "../../../../packages/core-nft/src/constraints/manager";
 import { Managers, Transactions } from "../../../../packages/crypto";
 import { NFTTransactionFactory } from "../../../helpers/nft-transaction-factory";
-import { CONSTRAINTS, NETWORK, NFT_ID, NFT_NAME } from "../__fixtures__";
+import { constraints, network, nftId, nftName } from "../__fixtures__";
 
 describe("core-nft > constraint manager", () => {
     // TODO: uns : find a way to prevent transaction register just for creating transaction
-    Managers.configManager.setFromPreset(NETWORK);
+    Managers.configManager.setFromPreset(network);
     Transactions.TransactionRegistry.registerTransactionType(NFTTransactions.NftMintTransaction);
     Transactions.TransactionRegistry.registerTransactionType(NFTTransactions.NftUpdateTransaction);
 
     let manager;
 
     beforeEach(() => {
-        manager = new ConstraintsManager(CONSTRAINTS);
+        manager = new ConstraintsManager(constraints);
     });
 
     describe("genesis constraint", () => {
         it("should pass when all genesis properties are set", () => {
             const properties = { genesisProp: "", propertyA: "", propertyB: "" };
-            const transaction = NFTTransactionFactory.nftMint(NFT_NAME, NFT_ID, properties).createOne();
+            const transaction = NFTTransactionFactory.nftMint(nftName, nftId, properties).createOne();
             expect(() => manager.applyGenesisPropertyConstraint(transaction)).not.toThrow();
         });
 
         it("should throw when one genesis property is missing", () => {
             const properties = { propertyA: "" };
-            const transaction = NFTTransactionFactory.nftMint(NFT_NAME, NFT_ID, properties).createOne();
+            const transaction = NFTTransactionFactory.nftMint(nftName, nftId, properties).createOne();
             expect(() => manager.applyGenesisPropertyConstraint(transaction)).toThrow(/genesis properties/);
         });
 
         it("should pass when nft has no properties", () => {
             const constraintsMock = {
                 unik: {
-                    name: NFT_NAME,
+                    name: nftName,
                     properties: {},
                 },
             };
             manager = new ConstraintsManager(constraintsMock);
             const properties = { propertyA: "", propertyB: "" };
-            const transaction = NFTTransactionFactory.nftMint(NFT_NAME, NFT_ID, properties).createOne();
+            const transaction = NFTTransactionFactory.nftMint(nftName, nftId, properties).createOne();
             expect(() => manager.applyGenesisPropertyConstraint(transaction)).not.toThrow();
         });
 
         it("should pass when nft has no genesis properties", () => {
             const configMock = {
                 unik: {
-                    name: NFT_NAME,
+                    name: nftName,
                     properties: {
                         foo: {},
                     },
@@ -57,7 +57,7 @@ describe("core-nft > constraint manager", () => {
             };
             manager = new ConstraintsManager(configMock);
             const properties = { propertyA: "", propertyB: "" };
-            const transaction = NFTTransactionFactory.nftMint(NFT_NAME, NFT_ID, properties).createOne();
+            const transaction = NFTTransactionFactory.nftMint(nftName, nftId, properties).createOne();
             expect(() => manager.applyGenesisPropertyConstraint(transaction)).not.toThrow();
         });
     });
@@ -67,14 +67,14 @@ describe("core-nft > constraint manager", () => {
             // tslint:disable-next-line: no-null-keyword
             nftsFindPropertyByKeyMock.mockReturnValue(null);
             const properties = { immutableProp: "1" };
-            const transaction = NFTTransactionFactory.nftUpdate(NFT_NAME, NFT_ID, properties).createOne();
+            const transaction = NFTTransactionFactory.nftUpdate(nftName, nftId, properties).createOne();
             return expect(manager.applyConstraints(transaction)).resolves.toBeUndefined();
         });
 
         it("should throw ConstraintError, property already set", () => {
             nftsFindPropertyByKeyMock.mockReturnValueOnce("1"); // db query
             const properties = { immutableProp: "2" };
-            const transaction = NFTTransactionFactory.nftUpdate(NFT_NAME, NFT_ID, properties).createOne();
+            const transaction = NFTTransactionFactory.nftUpdate(nftName, nftId, properties).createOne();
             return expect(manager.applyConstraints(transaction)).rejects.toThrow(/immutable/);
         });
     });
@@ -82,25 +82,25 @@ describe("core-nft > constraint manager", () => {
     describe("number constraint", () => {
         it("should pass, new value is in-bound", async () => {
             const properties = { numberProp: "1" };
-            const transaction = NFTTransactionFactory.nftUpdate(NFT_NAME, NFT_ID, properties).createOne();
+            const transaction = NFTTransactionFactory.nftUpdate(nftName, nftId, properties).createOne();
             return expect(manager.applyConstraints(transaction)).resolves.toBeUndefined();
         });
 
         it("should throw ConstraintError, new value is lower bound", async () => {
             const properties = { numberProp: "-1" };
-            const transaction = NFTTransactionFactory.nftUpdate(NFT_NAME, NFT_ID, properties).createOne();
+            const transaction = NFTTransactionFactory.nftUpdate(nftName, nftId, properties).createOne();
             return expect(manager.applyConstraints(transaction)).rejects.toThrow(/lower bound/);
         });
 
         it("should throw ConstraintError, new value is upper bound", async () => {
             const properties = { numberProp: "1000" };
-            const transaction = NFTTransactionFactory.nftUpdate(NFT_NAME, NFT_ID, properties).createOne();
+            const transaction = NFTTransactionFactory.nftUpdate(nftName, nftId, properties).createOne();
             return expect(manager.applyConstraints(transaction)).rejects.toThrow(/upper bound/);
         });
 
         it("should throw ConstraintError, new value is not a number", async () => {
             const properties = { numberProp: "helloworld" };
-            const transaction = NFTTransactionFactory.nftUpdate(NFT_NAME, NFT_ID, properties).createOne();
+            const transaction = NFTTransactionFactory.nftUpdate(nftName, nftId, properties).createOne();
             return expect(manager.applyConstraints(transaction)).rejects.toThrow(/not a number/);
         });
     });
@@ -108,13 +108,13 @@ describe("core-nft > constraint manager", () => {
     describe("enumeration constraint", () => {
         it("should pass, new value is in-bound", async () => {
             const properties = { enumProp: "foo" };
-            const transaction = NFTTransactionFactory.nftUpdate(NFT_NAME, NFT_ID, properties).createOne();
+            const transaction = NFTTransactionFactory.nftUpdate(nftName, nftId, properties).createOne();
             return expect(manager.applyConstraints(transaction)).resolves.toBeUndefined();
         });
 
         it("should throw ConstraintError, new value is not a number", async () => {
             const properties = { enumProp: "helloworld" };
-            const transaction = NFTTransactionFactory.nftUpdate(NFT_NAME, NFT_ID, properties).createOne();
+            const transaction = NFTTransactionFactory.nftUpdate(nftName, nftId, properties).createOne();
             return expect(manager.applyConstraints(transaction)).rejects.toThrow(/enumeration/);
         });
     });

53bd31f5fe37a8348c25d7ce915aeaa1c99895b1
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-12-06 15:33:29 +0100

    test(core-nft): add nft functional tests

---
+++ __tests__/functional/transaction-forging/__support__/nft-jest-matchers.ts
@@ -0,0 +1,93 @@
+import { Identities, Interfaces } from "@arkecosystem/crypto";
+import got from "got";
+import * as NftSupport from "./nft";
+
+const publicAPIEndpoint: string = "http://localhost:4003/api";
+
+declare global {
+    namespace jest {
+        // tslint:disable-next-line:interface-name
+        interface Matchers<R> {
+            toHaveValidNftPersistanceState(): Promise<R>;
+        }
+    }
+}
+
+const nftPropertiesAreUpdated = async (transaction: Interfaces.ITransactionData): Promise<any> => {
+    let pass: boolean = true;
+    let errorInfo: string;
+
+    const { asset } = transaction;
+    const nftAssets = asset.nft[NftSupport.nftName];
+    const { tokenId, properties } = nftAssets;
+
+    for (const [key, value] of Object.entries(properties)) {
+        try {
+            const { body } = await got.get(`${publicAPIEndpoint}/${NftSupport.nftName}s/${tokenId}/properties/${key}`);
+            const { data } = JSON.parse(body);
+
+            if (data !== value) {
+                // property should be updated
+                pass = false;
+                errorInfo = `(property ${key} is not updated)`;
+                break;
+            }
+            // property has been updated
+        } catch (error) {
+            if (error.response.status === 404) {
+                // handle 404 errors only
+
+                if (value !== null) {
+                    // property should be updated
+                    pass = false;
+                    errorInfo = `(property ${key} is not found)`;
+                    break;
+                }
+
+                // property has been deleted
+            } else {
+                throw error;
+            }
+        }
+    }
+
+    return {
+        pass,
+        errorInfo,
+    };
+};
+
+expect.extend({
+    toHaveValidNftPersistanceState: async (transaction: Interfaces.ITransactionData) => {
+        let pass: boolean = false;
+        let errorInfo: string = "";
+        const { senderPublicKey, id, asset, recipientId } = transaction;
+        const nftAssets = asset.nft[NftSupport.nftName];
+        const { tokenId, properties } = nftAssets;
+        const owner = recipientId ? recipientId : Identities.Address.fromPublicKey(senderPublicKey);
+
+        try {
+            const { body } = await got.get(`${publicAPIEndpoint}/${NftSupport.nftName}s/${tokenId}`);
+            const { data } = JSON.parse(body);
+
+            pass = data.id === tokenId && data.ownerId === owner && data.transactions.last.id === id;
+
+            if (pass && properties) {
+                const result = await nftPropertiesAreUpdated(transaction);
+                pass = result.pass;
+                if (!pass) {
+                    errorInfo = result.errorInfo;
+                }
+            } else {
+                errorInfo = `(id:${tokenId}/${data.id},owner:${owner}/${data.ownerId},tx:${id}/${data.transactions.last.id})`;
+            }
+        } catch (e) {
+            errorInfo = `(${e.message})`;
+        }
+
+        return {
+            pass,
+            message: () => `expected nft ${tokenId} ${this.isNot ? "not" : ""} to be persisted ${errorInfo}`,
+        };
+    },
+});
---
+++ __tests__/functional/transaction-forging/nft.test.ts
@@ -0,0 +1,102 @@
+import * as support from "./__support__";
+import * as NftSupport from "./__support__/nft";
+
+beforeAll(NftSupport.setUp);
+afterAll(support.tearDown);
+
+describe("Transaction Nft", () => {
+    it("should mint new unik token with genesis property", async () => {
+        const nftId = NftSupport.generateNftId();
+
+        const nftMint = NftSupport.nftMintTransaction(nftId, { type: "1" })
+            .withNetwork(NftSupport.network)
+            .withPassphrase(NftSupport.defaultPassphrase)
+            .createOne();
+
+        await expect(nftMint).toBeAccepted();
+        await support.snoozeForBlock(1);
+        await expect(nftMint.id).toBeForged();
+        await expect(nftMint).toHaveValidNftPersistanceState();
+    });
+
+    it("should add unik token property", async () => {
+        const nftId = NftSupport.generateNftId();
+        await NftSupport.mintAndWait(nftId);
+
+        const nftUpdate = NftSupport.nftUpdateTransaction(nftId, { foo: "foo" })
+            .withNetwork(NftSupport.network)
+            .withPassphrase(NftSupport.defaultPassphrase)
+            .createOne();
+
+        await expect(nftUpdate).toBeAccepted();
+        await support.snoozeForBlock(1);
+        await expect(nftUpdate.id).toBeForged();
+        await expect(nftUpdate).toHaveValidNftPersistanceState();
+    });
+
+    it("should update unik token property", async () => {
+        const nftId = NftSupport.generateNftId();
+        await NftSupport.mintAndWait(nftId);
+        await NftSupport.addPropertyAndWait(nftId, "aKey", "aValue");
+
+        const nftUpdate = NftSupport.nftUpdateTransaction(nftId, { aKey: "foo" })
+            .withNetwork(NftSupport.network)
+            .withPassphrase(NftSupport.defaultPassphrase)
+            .createOne();
+
+        await expect(nftUpdate).toBeAccepted();
+        await support.snoozeForBlock(1);
+        await expect(nftUpdate.id).toBeForged();
+        await expect(nftUpdate).toHaveValidNftPersistanceState();
+    });
+
+    it("should remove unik token property", async () => {
+        const nftId = NftSupport.generateNftId();
+        await NftSupport.mintAndWait(nftId);
+        await NftSupport.addPropertyAndWait(nftId, "aKey", "aValue");
+
+        // tslint:disable-next-line
+        const nftUpdate = NftSupport.nftUpdateTransaction(nftId, { aKey: null })
+            .withNetwork(NftSupport.network)
+            .withPassphrase(NftSupport.defaultPassphrase)
+            .createOne();
+
+        await expect(nftUpdate).toBeAccepted();
+        await support.snoozeForBlock(1);
+        await expect(nftUpdate.id).toBeForged();
+        await expect(nftUpdate).toHaveValidNftPersistanceState();
+    });
+
+    it("should not be accepted to delete nonexistent property", async () => {
+        const nftId = NftSupport.generateNftId();
+        await NftSupport.mintAndWait(nftId);
+
+        // tslint:disable-next-line
+        const nftUpdate = NftSupport.nftUpdateTransaction(nftId, { aKey: null })
+            .withNetwork(NftSupport.network)
+            .withPassphrase(NftSupport.defaultPassphrase)
+            .createOne();
+
+        // TODO: uns : replace by not to be accepted
+        await expect(nftUpdate).toBeAccepted();
+        await support.snoozeForBlock(1);
+        await expect(nftUpdate.id).toBeForged();
+        await expect(nftUpdate).toHaveValidNftPersistanceState();
+        // await expect(true).toBe(false)
+    });
+
+    it("should transfer unik token", async () => {
+        const nftId = NftSupport.generateNftId();
+        await NftSupport.mintAndWait(nftId);
+
+        const nftTransfer = NftSupport.nftTransferTransaction(nftId, "DQJjUU21VM2tjoHLwupeegv57iX4k7wucx")
+            .withNetwork(NftSupport.network)
+            .withPassphrase(NftSupport.defaultPassphrase)
+            .createOne();
+
+        await expect(nftTransfer).toBeAccepted();
+        await support.snoozeForBlock(1);
+        await expect(nftTransfer.id).toBeForged();
+        await expect(nftTransfer).toHaveValidNftPersistanceState();
+    });
+});

67c4875a89b8fa71d96db97b2303a7b366d96396
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-12-06 15:33:29 +0100

    test(core-nft): add nft functional tests

--- __tests__/functional/transaction-forging/__support__/nft.ts
+++ __tests__/functional/transaction-forging/__support__/nft.ts
@@ -1,12 +1,11 @@
+import { Crypto, Managers } from "@arkecosystem/crypto";
 import { Builders } from "@uns/core-nft-crypto";
 import * as path from "path";
 import { TransactionFactory } from "../../../helpers";
-import { defaultInclude, setUp as setup } from "./index";
+import { defaultInclude, setUp as setup, snoozeForBlock } from "./index";
+import "./nft-jest-matchers";
 
-const nftName: string = "unik";
-const nftId: string = "ee16f4b75f38f6e3d16635f72a8445e0ff8fbacfdfa8f05df077e73de79d6e4f";
-export const walletPassphrase: string =
-    "enrich account dirt wedding noise acquire pipe rescue link quality laugh rough";
+export const nftName: string = "unik";
 
 const token = "uns";
 export const network = "dalinet";
@@ -21,25 +20,71 @@ const delegatesFilePath = path.resolve(__dirname, config, "delegates.json");
 const packageFilePath = path.resolve(__dirname, corePackagePath, "package.json");
 
 // tslint:disable-next-line
-const secrets = require(delegatesFilePath).secrets;
-// tslint:disable-next-line
-const version = require(packageFilePath).version;
+export const secrets = require(delegatesFilePath).secrets;
 
-const options = {
-    network,
-    token,
-    config,
-    data,
-    version,
-};
+// it's genesis wallet
+export const genesisPassphrase: string =
+    "enrich account dirt wedding noise acquire pipe rescue link quality laugh rough";
+export const defaultPassphrase: string = genesisPassphrase;
 
 export const setUp = async (): Promise<void> => {
     process.env.CORE_P2P_PORT = "4002";
+    Managers.configManager.setFromPreset(network);
 
-    return setup(options, includes, secrets);
+    // tslint:disable-next-line
+    const version = require(packageFilePath).version;
+
+    return setup(
+        {
+            network,
+            token,
+            config,
+            data,
+            version,
+        },
+        includes,
+        secrets,
+    );
 };
 
-export const nftMintTransaction = (properties?: any) => {
-    const nftMintBuilder = new Builders.NftMintBuilder(nftName, nftId).properties(properties);
+export const nftMintTransaction = (id: string, properties) => {
+    const nftMintBuilder = new Builders.NftMintBuilder(nftName, id).properties(properties);
     return new TransactionFactory(nftMintBuilder);
+};
+
+export const nftUpdateTransaction = (id: string, properties) => {
+    const nftUpdateBuilder = new Builders.NftUpdateBuilder(nftName, id).properties(properties);
+    return new TransactionFactory(nftUpdateBuilder);
+};
+
+export const nftTransferTransaction = (id: string, recipient: string) => {
+    const nftTransferBuilder = new Builders.NftTransferBuilder(nftName, id).recipientId(recipient);
+    return new TransactionFactory(nftTransferBuilder);
+};
+
+export const generateNftId = () => {
+    return Crypto.HashAlgorithms.sha256(new Date().toISOString()).toString("hex");
+};
+
+export const mintAndWait = async nftId => {
+    const t = nftMintTransaction(nftId, { type: "1" })
+        .withNetwork(network)
+        .withPassphrase(defaultPassphrase)
+        .createOne();
+
+    await expect(t).toBeAccepted();
+    await snoozeForBlock(1);
+};
+
+export const addPropertyAndWait = async (nftId, key, value) => {
+    const properties = {};
+    properties[key] = value;
+
+    const t = nftUpdateTransaction(nftId, properties)
+        .withNetwork(network)
+        .withPassphrase(defaultPassphrase)
+        .createOne();
+
+    await expect(t).toBeAccepted();
+    await snoozeForBlock(1);
 };

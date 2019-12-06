e9d5d97e87ec6606b12068165685ee0f7e1ad5f0
Fabien TREGUER <fabien.treguer@spacelephant.org>
2019-12-06 11:54:58 +0100

    feat: add wallets/{id}/nfts api TP #1948

--- packages/core-api/src/core-nft/handlers/controller.ts
+++ packages/core-api/src/core-nft/handlers/controller.ts
@@ -57,4 +57,17 @@ export class NftController extends Controller {
             return Boom.badImplementation(error);
         }
     }
+
+    public async walletNfts(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+        try {
+            request.query.ownerId = request.params.id;
+
+            // @ts-ignore
+            const data = await request.server.methods.v2.nfts.search(request);
+
+            return super.respondWithCache(data, h);
+        } catch (error) {
+            return Boom.badImplementation(error);
+        }
+    }
 }

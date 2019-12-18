6c752a0ad398d20e3637765d1441ee9b9e504607
Pierre Seznec <pierre.seznec@spacelephant.org>
2019-12-18 16:09:09 +0100

    fix(uns-transaction): disclose revert

--- packages/core-nft/src/index.ts
+++ packages/core-nft/src/index.ts
@@ -1,6 +1,3 @@
 export * from "./plugin";
 export * from "./errors";
 export { nftRepository } from "./manager";
-
-// TODO: uns : quick fix to remove
-export { revertProperties } from "./transactions/handlers/helpers";
--- packages/uns-transactions/src/handlers/utils/helpers.ts
+++ packages/uns-transactions/src/handlers/utils/helpers.ts
@@ -1,23 +1,50 @@
 import { app } from "@arkecosystem/core-container";
 import { Interfaces } from "@arkecosystem/crypto";
+import { nftRepository } from "@uns/core-nft/";
 
 export const EXPLICIT_PROP_KEY = "explicitValues";
 
-export const getExplicitValue = (transaction: Interfaces.ITransactionData): string => {
-    return transaction.asset["disclose-demand"].payload.explicitValue.join(",");
+export const getCurrentTokenId = (transaction: Interfaces.ITransactionData): string => {
+    return transaction.asset["disclose-demand"].payload.sub;
 };
 
 export const setExplicitValue = async (transaction: Interfaces.ITransaction): Promise<any> => {
-    const tokenId = transaction.data.asset["disclose-demand"].payload.sub;
+    const tokenId = getCurrentTokenId(transaction.data);
     let explicitValues = transaction.data.asset["disclose-demand"].payload.explicitValue;
 
     const nftManager = app.resolvePlugin("core-nft");
     const currentValues = await nftManager.getProperty(tokenId, EXPLICIT_PROP_KEY);
-
-    if (currentValues?.value) {
-        const currentValuesArray = currentValues.value.split(",");
-        // concat current & new values and remove duplicates with Set
-        explicitValues = [...new Set(explicitValues.concat(currentValuesArray))];
-    }
+    explicitValues = manageNewExplicitValues(currentValues?.value.split(","), explicitValues);
     nftManager.manageProperties({ [EXPLICIT_PROP_KEY]: explicitValues.join(",") }, tokenId);
+};
+
+const manageNewExplicitValues = (currents: string[], newValues: string[]): string[] => {
+    if (currents?.length) {
+        // concat current & new values and remove duplicates with Set
+        newValues = [...new Set(newValues.concat(currents))];
+    }
+    return newValues;
+};
+
+export const revertExplicitValue = async (transaction: Interfaces.ITransactionData): Promise<void> => {
+    const tokenId = getCurrentTokenId(transaction);
+    const manager = app.resolvePlugin("core-nft");
+
+    const asset = { "disclose-demand": { payload: { sub: tokenId } } };
+    const transactions = await nftRepository().findTransactionsByAsset(
+        asset,
+        [transaction.type],
+        transaction.typeGroup,
+    );
+    let retrievedExplicits = [];
+
+    for (const tx of transactions.reverse()) {
+        if (tx.id === transaction.id) {
+            continue;
+        }
+        const txValues = tx.asset["disclose-demand"].payload.explicitValue;
+        retrievedExplicits = manageNewExplicitValues(retrievedExplicits, txValues);
+    }
+    // revert updated properties with last known value
+    await manager.manageProperties({ [EXPLICIT_PROP_KEY]: retrievedExplicits.join(",") }, tokenId);
 };

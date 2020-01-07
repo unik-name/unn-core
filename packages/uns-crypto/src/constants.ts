b5c066fddc92ed02fcda71cf1635abc46a31b9bd
Fabien TREGUER <fabien.treguer@spacelephant.org>
2020-01-07 18:37:27 +0100

    feat: add property key pattern constant

---
+++ packages/uns-crypto/src/constants.ts
@@ -0,0 +1,1 @@
+export const UNS_NFT_PROPERTY_KEY_REGEX = /[a-zA-Z0-9+-\.\/\_]+/;
--- packages/uns-crypto/src/index.ts
+++ packages/uns-crypto/src/index.ts
@@ -4,3 +4,4 @@ export * from "./interfaces";
 export * from "./enums";
 export * from "./crypto";
 export * from "./models";
+export * from "./constants";

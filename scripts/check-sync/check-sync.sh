1a38e02aca6a870aaa9d25bd00a8649f204106fa
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-02-06 16:44:49 +0100

    ci(dalinet): upgrade dalinet forgers one by one

--- scripts/check-sync/check-sync.sh
+++ scripts/check-sync/check-sync.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-curl -fsS --retry 3 https://hc-ping.com/89a3c92a-8f7b-402b-b5dc-982101cf1fd4/start
-
 set -e
 
 ip1=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' uns_dalinet_forger1)
@@ -22,18 +20,13 @@ height_diff_2_abs=$(echo ${height_diff_2#-})
 
 if ((  height_diff_1_abs > MAX_HEIGHT_DIFF || height_diff_2_abs > MAX_HEIGHT_DIFF ))
 then
-    curl -fsS --retry 3 https://hc-ping.com/89a3c92a-8f7b-402b-b5dc-982101cf1fd4/fail
-
     echo "Nodes sync error:"
     echo "height $ip1 : $height1"
     echo "height $ip2 : $height2"
     echo "height $ip3 : $height3"
+    exit 1
 else
     echo "Nodes are synced"
-    curl -fsS --retry 3 https://hc-ping.com/89a3c92a-8f7b-402b-b5dc-982101cf1fd4
 fi
-
-echo "-------"
-
 
 exit 0

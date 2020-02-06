52f53536d3a109241e4f393be00797600af0f592
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-02-06 16:44:49 +0100

    ci(dalinet): upgrade dalinet forgers one by one

--- scripts/upstream/update-upstream.sh
+++ scripts/upstream/update-upstream.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+wait_for_sync () {
+  timeout_sec=300
+  i=0
+  until /home/ubuntu/uns-core/scripts/check-sync/check-sync.sh >/dev/null; do
+    ((i=i+10))
+    if [ $i -gt $timeout_sec ]; then
+      echo "sync timeout reached"
+      break
+    fi
+    sleep 10
+  done
+}
+
 WORKDIR=~
 SOURCES_PATH=$WORKDIR/uns-core
 
@@ -27,10 +40,19 @@ fi
 cd $WORKDIR
 
 DOCKER_COMPOSE_FILE_PATH=$SOURCES_PATH/docker/integration/docker-compose.yml
+#upgrade forger 1
+echo upgrade forger 1
+docker-compose -f $DOCKER_COMPOSE_FILE_PATH up --no-deps --no-recreate --build -d forger1
 
-docker-compose -f $DOCKER_COMPOSE_FILE_PATH down
+#upgrade forger 2
+echo upgrade forger 2
+wait_for_sync
+docker-compose -f $DOCKER_COMPOSE_FILE_PATH up --no-deps  --no-recreate --build -d forger2
 
-docker-compose -f $DOCKER_COMPOSE_FILE_PATH up --build -d
+#upgrade forger 3
+echo upgrade forger 3
+wait_for_sync
+docker-compose -f $DOCKER_COMPOSE_FILE_PATH up --no-deps --no-recreate --build -d forger3
 
 docker image prune -f --filter "until=168h" # 7 days
 

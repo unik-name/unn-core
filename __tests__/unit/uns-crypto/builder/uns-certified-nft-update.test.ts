5668f0c144c1ba89001e75a96efea7fc435cf6e3
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-03-05 17:35:30 +0100

    tests: certified transaction schema

---
+++ __tests__/unit/uns-crypto/builder/uns-certified-nft-update.test.ts
@@ -0,0 +1,15 @@
+import { Managers, Transactions } from "@arkecosystem/crypto";
+import { CertifiedNftUpdateTransaction } from "@uns/crypto";
+import { testNftAssetSchema } from "../../core-nft/transactions/schemas-utils";
+import * as Fixtures from "../__fixtures__";
+import { testCertifiedBuilder } from "./uns-certified-nft-utils";
+
+describe("Uns Certified NFT update", () => {
+    Managers.configManager.setFromPreset(Fixtures.network);
+    Managers.configManager.setHeight(2);
+    Transactions.TransactionRegistry.registerTransactionType(CertifiedNftUpdateTransaction);
+
+    testCertifiedBuilder(Fixtures.unsCertifiedNftupdateTransaction());
+
+    testNftAssetSchema(CertifiedNftUpdateTransaction, Fixtures.unsCertifiedNftupdateTransaction());
+});

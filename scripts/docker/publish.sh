2b3579196eb229acfa567520a20569fd3b6c7472
Damien Lecan <damien.lecan@spacelephant.org>
2020-03-10 17:36:36 +0100

    ci: build automatically UNS docker image when git tags are published

--- scripts/docker/publish.sh
+++ scripts/docker/publish.sh
@@ -8,9 +8,16 @@ REPO="core"
 if [ "${1}" == "integration" ]; then
     REPO="d"$REPO
     TAG="latest"
+
+# for tags such as core-4.2.6
+elif [ ${1} == core* ]; then
+    TAG= echo "${1}" | cut -d- -f2 # keep only last part, the version with want to build
 else
+    echo "Unrecognized input tag, so publish this tag as-is: ${1}"
     TAG="${1}"
 fi
+
+echo "Tag to publish: ${TAG}"
 
 IMAGE=$ORG"/"$REPO
 

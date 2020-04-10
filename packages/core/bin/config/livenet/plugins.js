a1fedc55467da1e5f495f4a74befcc6519ac52e7
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-04-10 14:20:25 +0200

    Chore: Livenet init part 2

--- packages/core/bin/config/livenet/plugins.js
+++ packages/core/bin/config/livenet/plugins.js
@@ -1,5 +1,4 @@
-import * as sandbox_config from "../sandbox/plugins";
-
+sandbox_config = require("../sandbox/plugins");
 sandbox_config["@arkecosystem/core-p2p"].server.port = process.env.CORE_P2P_PORT || 4001;
 sandbox_config["@arkecosystem/core-forger"].hosts[0].port = process.env.CORE_P2P_PORT || 4001;
 

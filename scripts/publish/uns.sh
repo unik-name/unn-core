5de7c4b67311bd5af426a66ef35a2291871326a9
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-04-15 12:16:14 +0200

    chores: update uns publish script

--- scripts/publish/uns.sh
+++ scripts/publish/uns.sh
@@ -29,14 +29,15 @@ pushd packages/core
 yarn version --no-git-tag-version --new-version $version
 popd
 
-sed -i.bak "s/\"@uns\/core-nft-crypto\": \"\(.*\)\"/\"@uns\/core-nft-crypto\": \"^$version\"/g" "./plugins/nft/nft-transactions/package.json" "./packages/core-tester-cli/package.json"
-sed -i.bak "s/\"@uns\/crypto\": \"\(.*\)\"/\"@uns\/crypto\": \"^$version\"/g" "./plugins/uns/uns-transactions/package.json"
+sed -i.bak "s/\"@uns\/core-nft-crypto\": \"\(.*\)\"/\"@uns\/core-nft-crypto\": \"^$version\"/g" "./plugins/nft/nft-transactions/package.json" "./packages/core-tester-cli/package.json" "./plugins/uns/uns-transactions/package.json"
+sed -i.bak "s/\"@uns\/core-nft\": \"\(.*\)\"/\"@uns\/core-nft\": \"^$version\"/g" "./plugins/uns/uns-transactions/package.json"
+sed -i.bak "s/\"@uns\/crypto\": \"\(.*\)\"/\"@uns\/crypto\": \"^$version\"/g" "./plugins/uns/uns-transactions/package.json" "./plugins/nft/nft-transactions/package.json"
 
 # remove all `.bak` files
 find . -name "*.bak" -exec rm {} +
 
 if [[ $commit == '--commit' ]];then
-    git add -u
+    git add packages/ plugins/
     git commit -m "release: $version"
     git tag $version
 fi

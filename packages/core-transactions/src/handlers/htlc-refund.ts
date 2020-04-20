b898d711dae1c005cdceb08898fe61697cfba5b9
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-04-20 11:15:53 +0200

    feat: deactivate ark transactions

--- packages/core-transactions/src/handlers/htlc-refund.ts
+++ packages/core-transactions/src/handlers/htlc-refund.ts
@@ -32,7 +32,7 @@ export class HtlcRefundTransactionHandler extends TransactionHandler {
 
     public async isActivated(): Promise<boolean> {
         const milestone = Managers.configManager.getMilestone();
-        return milestone.aip11 === true && milestone.htlcEnabled === true;
+        return milestone.aip11 === true && milestone.htlcEnabled === true && this.isTransactionActivated();
     }
 
     public dynamicFee(context: IDynamicFeeContext): Utils.BigNumber {

6bdfa79e40bf0db77cac33cfca2dfa644da9c86a
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-04-21 12:50:39 +0200

    chore: split uns publish and bump scripts

--- scripts/publish/bump.sh
+++ scripts/publish/bump.sh
@@ -1,42 +1,43 @@
 #!/usr/bin/env bash
 
 version=$1
-
+commit=$2
 if [ -z "$version" ]
 then
     echo "empty uns version"
     exit 1
 fi
 
-pushd plugins/uns/ark-crypto
-yarn version --no-git-tag-version --new-version $version
-popd
+dev="-dev"
+if [[ $commit == "--bump" ]]; then
+    echo "bumping to version $version"
+fi
+if [[ $commit == "--release" ]]; then
+    dev=""
+    echo "releasing version $version"
+fi
 
-pushd plugins/nft/nft-crypto
-yarn version --no-git-tag-version --new-version $version
-popd
+uns_packages="plugins/uns/ark-crypto plugins/nft/nft-crypto plugins/nft/nft-transactions plugins/uns/uns-crypto plugins/uns/uns-transactions packages/core"
 
-pushd plugins/nft/nft-transactions
-yarn version --no-git-tag-version --new-version $version
-popd
+for package in $uns_packages; do
+ pushd $package
+ yarn version --no-git-tag-version --new-version $version
+ popd
+done
 
-pushd plugins/uns/uns-crypto
-yarn version --no-git-tag-version --new-version $version
-popd
-
-pushd plugins/uns/uns-transactions
-yarn version --no-git-tag-version --new-version $version
-popd
-
-pushd packages/core
-yarn version --no-git-tag-version --new-version $version
-sed -i.bak "s/\"@uns\/core-nft\": \"\(.*\)\"/\"@uns\/core-nft\": \"^$version-dev\"/g" "package.json"
-sed -i.bak "s/\"@uns\/uns-transactions\": \"\(.*\)\"/\"@uns\/uns-transactions\": \"^$version-dev\"/g" "package.json"
-popd
-
-sed -i.bak "s/\"@uns\/core-nft-crypto\": \"\(.*\)\"/\"@uns\/core-nft-crypto\": \"^$version-dev\"/g" "./plugins/nft/nft-transactions/package.json" "./packages/core-tester-cli/package.json" "./plugins/uns/uns-transactions/package.json" "plugins/uns/uns-badges/package.json"
-sed -i.bak "s/\"@uns\/core-nft\": \"\(.*\)\"/\"@uns\/core-nft\": \"^$version-dev\"/g" "./plugins/uns/uns-transactions/package.json" "plugins/uns/uns-badges/package.json"
-sed -i.bak "s/\"@uns\/crypto\": \"\(.*\)\"/\"@uns\/crypto\": \"^$version-dev\"/g" "./plugins/uns/uns-transactions/package.json" "./plugins/nft/nft-transactions/package.json" "plugins/uns/uns-badges/package.json"
+for dir in `grep --include=package.json -rl {packages,plugins} -e @uns`; do
+    sed -i.bak "s/\"@uns\/core-nft-crypto\": \"\(.*\)\"/\"@uns\/core-nft-crypto\": \"^$version$dev\"/g" $dir
+    sed -i.bak "s/\"@uns\/core-nft\": \"\(.*\)\"/\"@uns\/core-nft\": \"^$version$dev\"/g" $dir
+    sed -i.bak "s/\"@uns\/crypto\": \"\(.*\)\"/\"@uns\/crypto\": \"^$version$dev\"/g" $dir
+    sed -i.bak "s/\"@uns\/uns-transactions\": \"\(.*\)\"/\"@uns\/uns-transactions\": \"^$version$dev\"/g" $dir
+done
 
 # remove all `.bak` files
-find . -name "*.bak" -exec rm {} +
+find {packages,plugins} -name "*.bak" -exec rm {} +
+
+if [[ $commit == '--bump' || $commit == "--release" ]]; then
+    commit_msg="${commit:2}: $version$dev"
+    git add packages/ plugins/
+    git commit -m "$commit_msg"
+    git tag $version
+fi

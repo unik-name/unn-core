674b7300ca1b00bd24583620d7b3d901327d4361
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-04-21 11:40:05 +0200

    fix: livenet config loading

--- packages/core/bin/config/livenet/plugins.js
+++ packages/core/bin/config/livenet/plugins.js
@@ -1,4 +1,4 @@
-sandbox_config = require("../sandbox/plugins");
+sandbox_config = require("./sandbox-plugins");
 sandbox_config["@arkecosystem/core-p2p"].server.port = process.env.CORE_P2P_PORT || 4001;
 sandbox_config["@arkecosystem/core-forger"].hosts[0].port = process.env.CORE_P2P_PORT || 4001;
 

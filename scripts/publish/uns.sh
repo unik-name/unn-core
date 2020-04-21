5475e6c9f81d302198d03c8e3c3e6a68f861404e
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-04-21 12:50:39 +0200

    chore: split uns publish and bump scripts

--- scripts/publish/uns.sh
+++ scripts/publish/uns.sh
@@ -1,44 +1,16 @@
 #!/usr/bin/env bash
 
-version=$1
-commit=$2
-
-if [ -z "$version" ]
-then
-    echo "empty uns version"
-    exit 1
-fi
-
 pushd plugins/uns/ark-crypto
-yarn version --no-git-tag-version --new-version $version
-npm publish
+npm publish --dry-run
 popd
 
 pushd plugins/nft/nft-crypto
-yarn version --no-git-tag-version --new-version $version
-npm publish
+npm publish --dry-run
 popd
 
 pushd plugins/uns/uns-crypto
-yarn version --no-git-tag-version --new-version $version
-npm publish
+npm publish --dry-run
 yarn build
 popd
 
-pushd packages/core
-yarn version --no-git-tag-version --new-version $version
-popd
-
-sed -i.bak "s/\"@uns\/core-nft-crypto\": \"\(.*\)\"/\"@uns\/core-nft-crypto\": \"^$version\"/g" "./plugins/nft/nft-transactions/package.json" "./packages/core-tester-cli/package.json" "./plugins/uns/uns-transactions/package.json"
-sed -i.bak "s/\"@uns\/core-nft\": \"\(.*\)\"/\"@uns\/core-nft\": \"^$version\"/g" "./plugins/uns/uns-transactions/package.json"
-sed -i.bak "s/\"@uns\/crypto\": \"\(.*\)\"/\"@uns\/crypto\": \"^$version\"/g" "./plugins/uns/uns-transactions/package.json" "./plugins/nft/nft-transactions/package.json"
-
-# remove all `.bak` files
-find . -name "*.bak" -exec rm {} +
-
-if [[ $commit == '--commit' ]];then
-    git add packages/ plugins/
-    git commit -m "release: $version"
-    git tag $version
-fi
 

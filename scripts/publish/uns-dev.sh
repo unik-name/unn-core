06ccc0dbeb42095d3379a3c3817a78e81c00187b
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-05-14 12:03:57 +0200

    chores: use lerna canary release for publishing dev packages within CI

--- scripts/publish/uns-dev.sh
+++ scripts/publish/uns-dev.sh
@@ -16,41 +16,4 @@ if [[ -n "$CI" ]];then
     set -x
 fi
 
-
-pushd plugins/uns/ark-crypto
-actual_uns_version=$(grep '"version":' package.json | cut -d\" -f4)
-if [[ $actual_uns_version =~ "-" ]]; then
-    echo "Version format is not X.Y.Z ($actual_uns_version)"
-    exit 1
-fi
-
-echo "Bump package version..."
-DATE=$(date -u +%Y%m%d%H%M%S)
-new_version="${actual_uns_version}-dev.$DATE"
-echo "New version: ${new_version}"
-
-sed -i.bak "s/\"version\": \"\(.*\)\"/\"version\": \"$new_version\"/g" "./package.json"
-
-echo
-echo "Build and publish @uns/ark-crypto"
-echo
-npm publish --tag=dev $PUBLISH_OPTS
-popd
-
-echo
-echo "Build and publish @uns/core-nft-crypto"
-echo
-pushd plugins/nft/nft-crypto
-# It is mandatory to rebuild the module afterward to satisfy next modules dependency
-npm publish --tag=dev $PUBLISH_OPTS && yarn build
-popd
-
-echo
-echo "Build and publish @uns/uns-crypto"
-echo
-pushd plugins/uns/uns-crypto
-npm publish --tag=dev $PUBLISH_OPTS
-popd
-
-#restore backup
-mv "./plugins/uns/ark-crypto/package.json.bak" "./plugins/uns/ark-crypto/package.json"
+yarn publish:uns:dev --yes

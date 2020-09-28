dd35d4452211be5c04373e0d284f265cbfc5ec1c
Fabien TREGUER <fabien.treguer@spacelephant.org>
2020-09-28 17:57:21 +0200

    chore: enable builder to pass properties with null value

--- plugins/nft/nft-crypto/src/builders/abstract-nft.ts
+++ plugins/nft/nft-crypto/src/builders/abstract-nft.ts
@@ -3,7 +3,7 @@ import { NftBuilder } from "./nft";
 export abstract class AbstractNftWithPropertiesBuilder<
     T extends AbstractNftWithPropertiesBuilder<T>
 > extends NftBuilder<T> {
-    public properties(properties: { [_: string]: string }): this {
+    public properties(properties: { [_: string]: string | null }): this {
         this.data.asset.nft[this.nftName].properties = properties;
         return this;
     }

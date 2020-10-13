6c908cdd0c36ea8becf406d20e06fab8a5a761e3
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-10-13 17:54:32 +0200

    feat: token eco v2: add live demands in foundation wallet transactions

--- __tests__/integration/uns/core-api/handlers/wallets-v2.test.ts
+++ __tests__/integration/uns/core-api/handlers/wallets-v2.test.ts
@@ -4,13 +4,13 @@ import { Delegate } from "@arkecosystem/core-forger/src/delegate";
 import { Database } from "@arkecosystem/core-interfaces";
 import { Wallets } from "@arkecosystem/core-state";
 import { WalletManager } from "@arkecosystem/core-state/src/wallets";
-import { Identities, Networks, Utils } from "@arkecosystem/crypto";
+import { Identities, Managers, Networks, Utils } from "@arkecosystem/crypto";
 import { LIFE_CYCLE_PROPERTY_KEY, LifeCycleGrades, UnsTransactionType } from "@uns/crypto";
 import { getFoundationWallet } from "@uns/uns-transactions/src/handlers/utils/helpers";
 import * as support from "../../../../functional/transaction-forging/__support__";
 import * as NftSupport from "../../../../functional/transaction-forging/__support__/nft";
 import { NFTTransactionFactory } from "../../../../helpers/nft-transaction-factory";
-import * as Fixtures from "../../../../unit/uns-crypto/__fixtures__/";
+import * as Fixtures from "../../../../unit/uns-crypto/__fixtures__";
 import genesisBlock from "../../../../utils/config/dalinet/genesisBlock.json";
 import { utils } from "../../../core-api/utils";
 
@@ -23,6 +23,10 @@ beforeAll(async () => {
     database = app.resolvePlugin("database");
     walletManager = new WalletManager();
     stateBuilder = new StateBuilder(database.connection, walletManager);
+
+    // force v2 token Eco
+    Managers.configManager.setFromPreset(Fixtures.network);
+    Managers.configManager.getMilestone().unsTokenEcoV2 = true;
 });
 afterAll(support.tearDown);
 
@@ -36,20 +40,33 @@ describe("GET /wallets/:id/transactions", () => {
         senderWallet.publicKey = Identities.PublicKey.fromPassphrase(senderPassphrase);
         walletManager.reindex(senderWallet);
 
-        const properties = {
+        const mintProperties = {
             type: "1",
             UnikVoucherId: voucherId,
-            [LIFE_CYCLE_PROPERTY_KEY]: LifeCycleGrades.LIVE.toString(),
+            [LIFE_CYCLE_PROPERTY_KEY]: LifeCycleGrades.MINTED.toString(),
         };
 
-        const transaction = NFTTransactionFactory.nftCertifiedMint(
+        const mintTransaction = NFTTransactionFactory.nftCertifiedMint(
             tokenId,
             senderPassphrase,
             Fixtures.issUnikId,
             Fixtures.issPassphrase,
-            properties,
+            mintProperties,
             Utils.BigNumber.ZERO,
         ).createOne();
+
+        const UpdateTransaction = NFTTransactionFactory.nftCertifiedUpdate(
+            tokenId,
+            senderPassphrase,
+            Fixtures.issUnikId,
+            Fixtures.issPassphrase,
+            {
+                [LIFE_CYCLE_PROPERTY_KEY]: LifeCycleGrades.LIVE.toString(),
+            },
+            Utils.BigNumber.ZERO,
+        )
+            .withNonce(Utils.BigNumber.make(1))
+            .createOne();
 
         const delegate = new Delegate("delegate passphrase", Networks.dalinet.network);
 
@@ -61,7 +78,7 @@ describe("GET /wallets/:id/transactions", () => {
             },
             reward: Utils.BigNumber.ZERO,
         };
-        const block = delegate.forge([transaction], forgeOptions);
+        const block = delegate.forge([mintTransaction, UpdateTransaction], forgeOptions);
         await database.connection.saveBlock(block);
         await database.connection.buildWallets();
         await stateBuilder.run();
@@ -72,6 +89,6 @@ describe("GET /wallets/:id/transactions", () => {
         expect(response.data.data).toBeArray();
         expect(response.data.data.length).toEqual(1);
         utils.expectTransaction(response.data.data[0]);
-        expect(response.data.data[0].type).toEqual(UnsTransactionType.UnsCertifiedNftMint);
+        expect(response.data.data[0].type).toEqual(UnsTransactionType.UnsCertifiedNftUpdate);
     });
 });

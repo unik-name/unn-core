ff43f5375c6118794e66bfb37b31adf35af58573
Pierre Seznec <pierre.seznec@spacelephant.org>
2021-03-23 15:17:05 +0100

    feat: add event forwarder

--- packages/core/bin/config/livenet/plugins.js
+++ packages/core/bin/config/livenet/plugins.js
@@ -1,5 +1,6 @@
 sandbox_config = require("./sandbox-plugins");
 sandbox_config["@arkecosystem/core-p2p"].server.port = process.env.CORE_P2P_PORT || 4001;
 sandbox_config["@arkecosystem/core-forger"].hosts[0].port = process.env.CORE_P2P_PORT || 4001;
+sandbox_config["@foly/socket-event-forwarder"].port = process.env.EVENT_FORWARDER_PORT || 4101;
 
 module.exports = sandbox_config;

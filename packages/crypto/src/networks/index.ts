05b54409d481603402d3e4b2f0f254c470a8443e
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2021-04-21 12:20:08 +0200

    fix: mock ark networks

--- packages/crypto/src/networks/index.ts
+++ packages/crypto/src/networks/index.ts
@@ -3,6 +3,39 @@
 // export { testnet } from "./testnet";
 // export { unitnet } from "./unitnet";
 
+const mock = {
+    exceptions: {
+        blocks: [],
+        transactions: [],
+    },
+    milestones: [],
+    network: {
+        name: "",
+        messagePrefix: "",
+        bip32: {
+            public: 0,
+            private: 0,
+        },
+        pubKeyHash: 0,
+        nethash: "",
+        wif: 0,
+        slip44: 0,
+        aip20: 0,
+        client: {
+            token: "",
+            symbol: "",
+            explorer: "",
+        },
+    },
+};
+
+const devnet = mock;
+const mainnet = mock;
+const testnet = mock;
+const unitnet = mock;
+
+export { devnet, mainnet, testnet, unitnet };
+
 // TODO: uns : find better way to import new network
 export { dalinet } from "./dalinet";
 export { sandbox } from "./sandbox";

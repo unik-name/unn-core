e3057c0db6f536bf1ea918e4f86208f3780b0bc5
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2021-04-21 12:26:41 +0200

    fix: mock is dalinet

--- packages/crypto/src/networks/index.ts
+++ packages/crypto/src/networks/index.ts
@@ -3,40 +3,16 @@
 // export { testnet } from "./testnet";
 // export { unitnet } from "./unitnet";
 
-const mock = {
-    exceptions: {
-        blocks: [],
-        transactions: [],
-    },
-    milestones: [],
-    network: {
-        name: "",
-        messagePrefix: "",
-        bip32: {
-            public: 0,
-            private: 0,
-        },
-        pubKeyHash: 0,
-        nethash: "",
-        wif: 0,
-        slip44: 0,
-        aip20: 0,
-        client: {
-            token: "",
-            symbol: "",
-            explorer: "",
-        },
-    },
-};
+import { dalinet } from "./dalinet";
 
-const devnet = mock;
-const mainnet = mock;
-const testnet = mock;
-const unitnet = mock;
+const devnet = dalinet;
+const mainnet = dalinet;
+const testnet = dalinet;
+const unitnet = dalinet;
 
 export { devnet, mainnet, testnet, unitnet };
 
 // TODO: uns : find better way to import new network
-export { dalinet } from "./dalinet";
+// export { dalinet } from "./dalinet";
 export { sandbox } from "./sandbox";
 export { livenet } from "./livenet";

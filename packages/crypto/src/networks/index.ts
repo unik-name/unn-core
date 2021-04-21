05b407f695736015e48e227e7b8cd71f86394c17
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2021-04-21 12:10:56 +0200

    feat: remove ark networks exports

--- packages/crypto/src/networks/index.ts
+++ packages/crypto/src/networks/index.ts
@@ -1,7 +1,7 @@
-export { devnet } from "./devnet";
-export { mainnet } from "./mainnet";
-export { testnet } from "./testnet";
-export { unitnet } from "./unitnet";
+// export { devnet } from "./devnet";
+// export { mainnet } from "./mainnet";
+// export { testnet } from "./testnet";
+// export { unitnet } from "./unitnet";
 
 // TODO: uns : find better way to import new network
 export { dalinet } from "./dalinet";

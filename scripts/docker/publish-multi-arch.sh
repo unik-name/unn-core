630923e36d8b3b15aa19a9f1260c3a23caec78b9
qvstr <quentin.sauvetre@protonmail.com>
2021-08-06 11:29:28 +0200

    feat: add multi arch

---
+++ scripts/docker/publish-multi-arch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+echo $IMAGE" "$COMMIT
+. ./scripts/docker/init.sh
+
+echo "Docker publish multi arch for $COMMIT or $TARGET"
+
+DOCKER_BUILDKIT=1
+docker manifest create "$IMAGE":"$COMMIT" --amend "$IMAGE":"$COMMIT"-x86_64 --amend "$IMAGE":"$COMMIT"-aarch64
+docker manifest push "$IMAGE":"$COMMIT"
+
+docker manifest create "$IMAGE":latest --amend "$IMAGE":"$COMMIT"-x86_64 --amend "$IMAGE":"$COMMIT"-aarch64
+docker manifest push "$IMAGE":latest
